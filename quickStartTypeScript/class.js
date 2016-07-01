69e8e4654f2b98058c475a3c7232d1bf62a5f304
blackknight <blackknight7.h@gmail.com>
2016-07-01 16:29:37 +0800

    Create class.

---
+++ quickStartTypeScript/class.js
@@ -0,0 +1,32 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Department = (function () {
+    function Department(name) {
+        this.name = name;
+    }
+    Department.prototype.printName = function () {
+        console.log('Department name: ' + this.name);
+    };
+    return Department;
+}());
+var AccountingDepartment = (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment() {
+        _super.call(this, 'Accounting and Auditing'); // constructors in derived classes must call super()
+    }
+    AccountingDepartment.prototype.printMeeting = function () {
+        console.log('The Accounting Department meets each Monday at 10am.');
+    };
+    AccountingDepartment.prototype.generateReports = function () {
+        console.log('Generating accounting reports...');
+    };
+    return AccountingDepartment;
+}(Department));
+var department; // ok to create a reference to an abstract type
+department = new AccountingDepartment(); // ok to create and assign a non-abstract subclass
+department.printName();
+department.printMeeting();
+//# sourceMappingURL=class.js.map
